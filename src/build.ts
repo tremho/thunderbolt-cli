9f0946afe60799d2f950244a8cae4d30dd6fb265
Tremho <steve@ohmert.com>
2021-09-30 12:35:27 -0700

    going all default for TsChecker webpack plugin options

--- src/build.ts
+++ src/build.ts
@@ -121,10 +121,7 @@ function doWebpackBuild() {
             },
             // these don't seem to be doing anything for me.
             plugins: [
-                new ForkTsCheckerWebpackPlugin({
-                    typescript:true
-                    // eslint: {enabled:true, files: "**/*.ts"}  // no eslint support
-                }),
+                new ForkTsCheckerWebpackPlugin(),
                 new ForkTsCheckerNotifierWebpackPlugin({ title: 'TypeScript', excludeWarnings: false }),
                 // new HtmlWebpackPlugin({
                 //     inject: true,
