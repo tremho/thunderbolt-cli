d196fe1e816d179b05a51f52ec3e5c1f7023d269
Tremho <steve@ohmert.com>
2021-11-07 11:05:49 -0800

    Back to Selenium 7

--- src/test.ts
+++ src/test.ts
@@ -77,7 +77,7 @@ export function doTest() {
         process.chdir(workingDirectoryOfOurApp)
 
         let builder = new Builder()
-            .forBrowser('electron')
+            .forBrowser('chrome')
             .setChromeOptions(copts)
             builder.build().then((driver:any) => {
                 console.log('driver is ready', driver)
