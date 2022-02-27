ad63d11ac1f011ef5113a067cb950a8d1e46efd9
Tremho <steve@ohmert.com>
2022-02-27 10:53:11 -0800

    More canvas shoehorning

--- src/tbFiles/PageWriterNS.ts
+++ src/tbFiles/PageWriterNS.ts
@@ -104,6 +104,7 @@ export function writeNativeScriptPage(info:PageInfo, srcpath:string, outDir:stri
     export function onNavigatedTo() {
         AppCore.getTheApp().launchActivity("${id}",activity)
     }
+    export function canvasReady(arg) { activity.canvasReady(arg) }
     `
     src = path.join(srcpath, `${id}-page.jvpg`)
     dest = path.join(outDir, `${id}-page.ts`)
